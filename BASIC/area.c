9defe37f573ebfe66a6b4de0664bc7dc0e71a2cf
adityasalve44 <aditya.salve.dev44@gmail.com>
2024-04-01 15:54:17 +0530

    modified:   .gitignore 	modified:   BASIC/area.c

--- BASIC/area.c
+++ BASIC/area.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 int main()
 {
-    float r;
-    float aoc;
-    printf("\n enter radius");
-    scanf("%f", &r);
-    aoc = r * r * 3.14;
-    printf("\narea of circle=%f\n", aoc);
+    int radius = 5;
+    float pi =3.14;
+    float area;
+    area = pi* radius*radius;
+    printf("%f",area);
     return 0;
 }
+
+// area of circle = 3.14 *radius*radius;
+
+/**
+ *  variables -- names which are used to hold values
+ *          variables can hold only one value at a time
+ *          but they can have multiple values throughout the program
+ *  * data types: int char float double
+ *
+ * Syntax:      data_type variable_name ;  --variable declaration
+ *
+ *      ex: int a;
+ *
+ *   Type specifiers:
+ *     %i/%d --int , %f --float, %f --double, %c --char.
+ * 
+ *  = assignment operator
+ *     lhs = rhs
+ *      
+ */
