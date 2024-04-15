93c4f4d9c6042bf2c6444154af58b70d35923ecd
adityasalve44 <aditya.salve.dev44@gmail.com>
2024-04-15 15:43:50 +0530

    arithmatic operators

--- basic_codes/addition.c
+++ basic_codes/addition.c
@@ -1,11 +1,24 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+int main(int a, int b)
 {
-    int a=35;
-    int b=45;
-    int c;
-    c=a+b;
-    printf("Addition:%d\n",c);
+    int sum;
+    printf("\nEnter two values:");
+    scanf("%d %d", &a, &b);
+    printf("a=%d b=%d", a, b);
+    sum = a + b;
+    printf("\nAddition:%d",sum);
     return 0;
-
 }
+
+/**
+ * Operators:
+ * 1.Arithmatic: + - * / %
+ * 2.Logical    : &&(AND) ||(OR) !(NOT)
+ * 3.Relational : < > == <= >= !=
+ * 4.Bit-Wise : & | >> << ^
+ * 5.Assignment: = += -= *= /= %=
+ * 6.Increment/Decrement: ++ --
+ * 7.Other : sizeof  ? : & *
+ *
+ */
+// scanf("type_specifier1 type_specifier2 type_specifier3 ...type_specifierN",variable_name1,variable_name2,variable_name3,..variable_nameN);
--- intermediate/swapping.c
+++ intermediate/swapping.c
@@ -3,22 +3,24 @@
 int swap(int x, int y)
 {
     int temp;
-    temp=x;
-    x=y;
-    y=temp;
+    temp = x;
+    x = y;
+    y = temp;
     printf("\n a=%d b=%d", x, y);
-    return 0;
+    return a + b;
 }
 int main()
 {
-    int a,b;
+    int add;
+    int a, b;
     printf("enter two values");
     scanf("%d %d", &a, &b);
     printf("\n a=%d b=%d", a, b);
-    swap(a,b);  //function call
+    add=swap(a, b); // function call
+    printf("\nAddition=%d",add)
     return 0;
 }
 /**
  *   a_a
  * thisisthenameofmyfuncion()
-*/
+ */
